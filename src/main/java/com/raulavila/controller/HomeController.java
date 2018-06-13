685d29895ac5e300b08a421eedf0c8048c8b3227
Raul Avila <raul@raulavila.com>
2018-06-13 12:19:31 +0100

    Added REST and view examples

--- src/main/java/com/raulavila/controller/HomeController.java
+++ src/main/java/com/raulavila/controller/HomeController.java
@@ -2,17 +2,28 @@ package com.raulavila.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
+	private String message = "Hello World (body)";
+
+	@RequestMapping(value="/helloWorld", produces = "text/html")
+	@ResponseBody
+	public String helloBody() throws IOException{
+		return "<h1>" + message + "</h1>";
+	}
+
+	@RequestMapping(value="/helloWorldView")
+	public ModelAndView helloView() throws IOException{
 		return new ModelAndView("home");
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -8,6 +8,6 @@
         <title>Home</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Hello World (view)</h1>
     </body>
 </html>
